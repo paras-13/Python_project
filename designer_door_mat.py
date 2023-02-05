4108dc049bf1337bd663df24b6141ea2e0cd4057
Paras Upadhyay <paras13upadhyay@gmail.com>
2023-02-05 13:19:36 +0530

    Designer door mat

---
+++ designer_door_mat.py
@@ -0,0 +1,26 @@
+# Hackerrank problem
+# Program to print designer carpet.
+# Here row is and odd umber and col is third multiple of row
+# For :eg--> 9 27, 11 33, 7 21
+
+row,col = int(input()),int(input())         # Defining variables
+ch = ".|."
+wel = "WELCOME"
+
+def upper(row,col,ch):                  # Function to print upper layer design
+    for i in range(row//2):
+        exp = ch*(2*i+1)
+        print(exp.center(col,"-"))
+
+def middle(row,col,wel):                # Function to print middle level design
+    print(wel.center(col,"-"))
+
+def bottom(row,col,ch):                 # Function to print lower level design
+    for i in range(row//2):
+        exp = ch*((row-2)-2*i)
+        print(exp.center(col,"-"))
+
+if __name__ == "__main__":              # Calling functions
+    upper(row,col,ch)
+    middle(row,col,wel)
+    bottom(row,col,ch)
